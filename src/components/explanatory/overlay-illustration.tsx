a2d3ad3c677fc31d50b9d7d14512f51e2e485c90
Ilyas Irfan <zukazine007@gmail.com>
2024-10-29 10:16:50 +0700

    add overlay illustration

--- src/components/explanatory/overlay-illustration.tsx
+++ src/components/explanatory/overlay-illustration.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import React from 'react'
 import { useState } from 'react';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa'
 
 export const OverlayIllustration = () => {
   const [click, setClick] = useState(true)
@@ -9,52 +9,54 @@ export const OverlayIllustration = () => {
 	return (
 		<button onClick={() => { setClick(!click) }}>
 			<div style={{perspective: 600}}>
-				<div className="group flex relative w-24 h-1" style={{
-					transformStyle: 'preserve-3d',
-					transform: 'rotateX(-30deg) rotateY(-45deg)',
-				}}>
+				<div 
+					className="group flex relative w-24 h-1" 
+					style={{
+						transformStyle: 'preserve-3d',
+						transform: 'rotateX(-30deg) rotateY(-45deg)',
+					}}
+				>
 					{click ?
-					<>
-						<div className="absolute flex items-center justify-center w-full h-24 bg-neutral-200 group-hover:bg-white border border-gray-300 transition-all duration-300" style={{
-							transform: 'rotateX(90deg) translateZ(3rem)'
-						}}>
-							<img
-								alt="eye icon"
-								src="open.png"
-								className="max-w-8 opacity-0 group-hover:opacity-100 transition-all duration-300"
-								style={{
-									transform: 'rotateZ(135deg) rotateY(45deg)'
-								}}
+						<>
+							<div 
+								className="absolute flex items-center justify-center w-full h-24 bg-neutral-200 group-hover:bg-white border border-gray-300 transition-all duration-300" 
+								style={{ transform: 'rotateX(90deg) translateZ(3rem)' }}
+							>
+								<FaRegEye
+									className="max-w-8 opacity-0 group-hover:opacity-100 transition-all duration-300"
+									style={{
+										transform: 'rotateZ(135deg) rotateY(45deg)'
+									}}
+								/>
+							</div>
+							<div 
+								className="absolute w-full h-[2px] group-hover:h-[10px] bg-white border border-black transition-all duration-300" 
+								style={{ transform: 'translateZ(3rem)' }}
+								/>
+							<div 
+								className="absolute w-full h-[2px] group-hover:h-[10px] bg-white border border-black transition-all duration-300" 
+								style={{ transform: 'rotateY(90deg) translateZ(3rem)' }} 
 							/>
-						</div>
-						<div className="absolute w-full h-[2px] group-hover:h-[10px] bg-white border border-black transition-all duration-300" style={{
-							transform: 'translateZ(3rem)'
-						}}></div>
-						<div className="absolute w-full h-[2px] group-hover:h-[10px] bg-white border border-black transition-all duration-300" style={{
-							transform: 'rotateY(90deg) translateZ(3rem)'
-						}}></div>
-					</>
+						</>
 					:
-					<>
-						<div className="absolute flex items-center justify-center w-full h-24 bg-neutral-800 group-hover:bg-neutral-600 border border-gray-300 hover:border-white transition-all duration-300" style={{
-							transform: 'rotateX(90deg) translateZ(3rem)'
-						}}>
-							<img
-								src="close.png"
-								alt="eye icon"
-								className="max-w-8 opacity-0 group-hover:opacity-100"
-								style={{
-									transform: 'rotateZ(135deg) rotateY(45deg)'
-								}}
-							/>
-						</div>
-						<div className="absolute w-full h-[2px] group-hover:h-[10px] bg-neutral-800 border border-black transition-all duration-300" style={{
-							transform: 'translateZ(3rem)'
-						}}></div>
-						<div className="absolute w-full h-[2px] group-hover:h-[10px] bg-neutral-800 border border-black transition-all duration-300" style={{
-							transform: 'rotateY(90deg) translateZ(3rem)'
-						}}></div>
-					</>
+						<>
+							<div 
+								className="absolute flex items-center justify-center w-full h-24 bg-neutral-800 group-hover:bg-neutral-600 border border-gray-300 hover:border-white transition-all duration-300" 
+								style={{ transform: 'rotateX(90deg) translateZ(3rem)' }}>
+								<FaRegEyeSlash
+									className="max-w-8 opacity-0 group-hover:opacity-100"
+									style={{
+										transform: 'rotateZ(135deg) rotateY(45deg)'
+									}}
+								/>
+							</div>
+							<div 
+								className="absolute w-full h-[2px] group-hover:h-[10px] bg-neutral-800 border border-black transition-all duration-300" 
+								style={{ transform: 'translateZ(3rem)' }} />
+							<div 
+								className="absolute w-full h-[2px] group-hover:h-[10px] bg-neutral-800 border border-black transition-all duration-300" 
+								style={{ transform: 'rotateY(90deg) translateZ(3rem)' }}/>
+						</>
 					}
 				</div>
 			</div>
