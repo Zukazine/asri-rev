5ba90ef358317a7d1017a15b21867f62c0bc89b8
Ilyas Irfan <zukazine007@gmail.com>
2024-10-30 13:47:23 +0700

    explanatory rev

--- src/components/explanatory/layers.tsx
+++ src/components/explanatory/layers.tsx
@@ -6,19 +6,51 @@ export const Layers = () => {
     <div className="grid grid-cols-2 h-[300vh] relative">
       <div className="sticky top-0 left-0 w-full h-screen ">
         <div className="h-full flex items-center justify-center">
-          <OverlayIllustration />
+          {/* <OverlayIllustration /> */}
+          <div className='size-48'>
+            <img 
+              src='/image/overlay.png'
+            />
+          </div>
         </div>
       </div>
       <div className="w-full h-screen">
         <div className="grid grid-rows-3 ">
-          <div className="text-white min-h-screen flex items-center justify-center">
-            KONTEN 1
+          <div className="text-white min-h-screen flex flex-col justify-center gap-6">
+            <p className='text-4xl font-bold font-gilroy text-gray-600'>
+              # Layer 1
+            </p>
+            <img 
+              src='/explanatory/layer-1.jpeg'
+              className='w-[420px] rounded-3xl'
+            />
+            <p className='text-[15px] w-[90%] text-gray-600'>
+              VBI digunakan untuk mengevaluasi kesehatan dan distribusi vegetasi di suatu area. Indeks ini dapat membantu dalam memahami kondisi vegetasi, seperti tingkat pertumbuhan dan keberagaman, serta dampaknya terhadap ekosistem.
+            </p>
           </div>
-          <div className="text-white min-h-screen flex items-center justify-center">
-            KONTEN 2
+          <div className="text-white min-h-screen flex flex-col justify-center gap-6">
+            <p className='text-4xl font-bold font-gilroy text-gray-600'>
+              # Layer 2
+            </p>
+            <img 
+              src='/explanatory/layer-2.jpeg'
+              className='w-[420px] rounded-3xl'
+            />
+            <p className='text-[15px] w-[90%] text-gray-600'>
+              WBI memberikan informasi mengenai keseimbangan air di suatu kawasan. Indeks ini mengukur interaksi antara input air (seperti curah hujan) dan output (seperti evaporasi dan aliran permukaan). Analisis WBI sangat penting untuk pengelolaan sumber daya air dan pertanian.
+            </p>
           </div>
-          <div className="text-white min-h-screen flex items-center justify-center">
-            KONTEN 3
+          <div className="text-white min-h-screen flex flex-col justify-center gap-6">
+            <p className='text-4xl font-bold font-gilroy text-gray-600'>
+              # Layer 3
+            </p>
+            <img 
+              src='/explanatory/layer-3.jpeg'
+              className='w-[420px] rounded-3xl'
+            />
+            <p className='text-[15px] w-[90%] text-gray-600'>
+              SMI digunakan untuk mengukur kelembapan tanah, yang sangat penting untuk pertumbuhan tanaman dan kesehatan tanah. Indeks ini memberikan wawasan tentang kemampuan tanah dalam menahan air, yang berpengaruh terhadap irigasi dan ketahanan tanaman terhadap kekeringan.
+            </p>
           </div>
         </div>
       </div>
